81d8baa6b088185ae1d97bac3bbb4c6e94c36705
CN-microsoftdongzh <zhang.dong@mcdmvp.onmicrosoft.com>
2023-08-19 16:10:02 +0800

    Initial commit

---
+++ src/main/java/com/zd0094/masterservice/MasterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zd0094.masterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zd0094/masterservice/MasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zd0094.masterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
